70b314f40cda935a6b15985588ba2f32114950cb
Andrea Bruno <andreabrunoxy@gmail.com>
2021-06-05 12:03:40 +0200

    Modify Mobile Button

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,9 +1,19 @@
-import styles from './Button.module.css';
+import styles from "./Button.module.css";
+// /*Fontawesome*/
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// Import the FontAwesomeIcon component
+import { faPhone } from "@fortawesome/free-solid-svg-icons"; // import the icons you need
 
-export const Button = ({ settings = { text: '', type: 'button', customClass: '' } }) => {
-  const { customClass, text, type } = settings;
+export const Button = ({
+  settings = { text: "", type: "button", customClass: "", customClassText: "" }
+}) => {
+  const { customClass, customClassText, text, type } = settings;
 
   return (
-    <button className={`${styles['button']} ${customClass}`} type={type}>{text}</button>
+    <button className={`${styles["button"]} ${customClass}`} type={type}>
+      {" "}
+      <FontAwesomeIcon icon={faPhone} size="lg" />{" "}
+      <span className={`${customClassText}`}>{text}</span>
+    </button>
   );
-}
+};
--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,8 +1,8 @@
-import Image from 'next/image';
+import Image from "next/image";
 /* Components */
-import { Button } from '../Button/Button';
+import { Button } from "../Button/Button";
 /* Styles */
-import styles from './Header.module.css';
+import styles from "./Header.module.css";
 
 // /* Styles */
 // import styles from "./Header.module.css";
@@ -24,142 +24,146 @@ import styles from './Header.module.css';
 // import { Bounce } from "react-reveal";
 
 const Header = () => {
-//   printHello();
+  //   printHello();
 
-//   const [menuOpen, setMenuOpen] = useState(false);
-//   const [menuIcon, setMenuIcon] = useState(faBars);
+  //   const [menuOpen, setMenuOpen] = useState(false);
+  //   const [menuIcon, setMenuIcon] = useState(faBars);
 
-//   function showMenu() {
-//     if (!menuOpen) {
-//       setMenuOpen(true);
-//       setMenuIcon(faTimes);
-//     } else {
-//       setMenuOpen(false);
-//       setMenuIcon(faBars);
-//     }
-//   }
+  //   function showMenu() {
+  //     if (!menuOpen) {
+  //       setMenuOpen(true);
+  //       setMenuIcon(faTimes);
+  //     } else {
+  //       setMenuOpen(false);
+  //       setMenuIcon(faBars);
+  //     }
+  //   }
 
-//   return (
-//     <>
-//       <div className={`${styles["info"]}`}>
-//         <h4 className={`${styles["info__tel"]}`}>
-//           <FontAwesomeIcon icon={faPhone} /> tel: 000 111 222 33
-//         </h4>
-//       </div>
-//       <header className={`${styles["header"]}`}>
-//         <div>
-//           <Link href="/">
-//             <h2>
-//               <a className={`${styles["header__logo"]}`}>CE.MA Impianti</a>
-//             </h2>
-//           </Link>
-//         </div>
+  //   return (
+  //     <>
+  //       <div className={`${styles["info"]}`}>
+  //         <h4 className={`${styles["info__tel"]}`}>
+  //           <FontAwesomeIcon icon={faPhone} /> tel: 000 111 222 33
+  //         </h4>
+  //       </div>
+  //       <header className={`${styles["header"]}`}>
+  //         <div>
+  //           <Link href="/">
+  //             <h2>
+  //               <a className={`${styles["header__logo"]}`}>CE.MA Impianti</a>
+  //             </h2>
+  //           </Link>
+  //         </div>
 
-//         <ul className={`${styles["nav"]}`}>
-//           <li className={`${styles["nav__item"]}`}>
-//             <Link href="#">
-//               <a>Home</a>
-//             </Link>
-//           </li>
-//           <li className={`${styles["nav__item"]}`}>
-//             <Link href="#servizi">
-//               <a>Servizi</a>
-//             </Link>
-//           </li>
-//           <li className={`${styles["nav__item"]}`}>
-//             <Link href="#">
-//               <a>Chi Siamo</a>
-//             </Link>
-//           </li>
-//           <li className={`${styles["nav__item"]}`}>
-//             <Link href="#">
-//               <a>Contatti</a>
-//             </Link>
-//           </li>
-//         </ul>
-//         {/* call me button */}
-//         <Bounce>
-//           <div className={`${styles["btn-call"]}`}>
-//             <Link href="tel:0111122333">
-//               <div>
-//                 <FontAwesomeIcon icon={faPhone} size="lg" />
-//                 <a></a>
-//               </div>
-//             </Link>
-//           </div>
-//         </Bounce>
+  //         <ul className={`${styles["nav"]}`}>
+  //           <li className={`${styles["nav__item"]}`}>
+  //             <Link href="#">
+  //               <a>Home</a>
+  //             </Link>
+  //           </li>
+  //           <li className={`${styles["nav__item"]}`}>
+  //             <Link href="#servizi">
+  //               <a>Servizi</a>
+  //             </Link>
+  //           </li>
+  //           <li className={`${styles["nav__item"]}`}>
+  //             <Link href="#">
+  //               <a>Chi Siamo</a>
+  //             </Link>
+  //           </li>
+  //           <li className={`${styles["nav__item"]}`}>
+  //             <Link href="#">
+  //               <a>Contatti</a>
+  //             </Link>
+  //           </li>
+  //         </ul>
+  //         {/* call me button */}
+  //         <Bounce>
+  //           <div className={`${styles["btn-call"]}`}>
+  //             <Link href="tel:0111122333">
+  //               <div>
+  //                 <FontAwesomeIcon icon={faPhone} size="lg" />
+  //                 <a></a>
+  //               </div>
+  //             </Link>
+  //           </div>
+  //         </Bounce>
 
-//         <div onClick={showMenu} className={`${styles["toggle-menu"]}`}>
-//           <FontAwesomeIcon icon={menuIcon} size="lg" />
-//         </div>
-//       </header>
-//       {/*dropdown Menu*/}
-//       <div
-//         className={`${styles["dropdown-menu"]} ${
-//           menuOpen
-//             ? `${styles["dropdown-menu--is-open"]}`
-//             : `${styles["dropdown-menu--is-close"]}`
-//         } `}
-//       >
-//         <li className={`${styles["nav__item--mobile"]}`}>
-//           <Link href="#">
-//             <div onClick={showMenu}>
-//               <FontAwesomeIcon icon={faHome} />
-//               <a> Home</a>
-//             </div>
-//           </Link>
-//         </li>
-//         <li className={`${styles["nav__item--mobile"]}`}>
-//           <Link href="#servizi">
-//             <div onClick={showMenu}>
-//               <FontAwesomeIcon icon={faBriefcase} />
-//               <a> Servizi</a>
-//             </div>
-//           </Link>
-//         </li>
-//         <li className={`${styles["nav__item--mobile"]}`}>
-//           <Link href="#">
-//             <div>
-//               <FontAwesomeIcon icon={faUserFriends} />
-//               <a> Chi siamo</a>
-//             </div>
-//           </Link>
-//         </li>
-//         <li className={`${styles["nav__item--mobile"]}`}>
-//           <Link href="#">
-//             <div>
-//               <FontAwesomeIcon icon={faEnvelopeOpenText} />
-//               <a> Contatti</a>
-//             </div>
-//           </Link>
-//         </li>
-//       </div>
-//     </>
-//   );
-// };
+  //         <div onClick={showMenu} className={`${styles["toggle-menu"]}`}>
+  //           <FontAwesomeIcon icon={menuIcon} size="lg" />
+  //         </div>
+  //       </header>
+  //       {/*dropdown Menu*/}
+  //       <div
+  //         className={`${styles["dropdown-menu"]} ${
+  //           menuOpen
+  //             ? `${styles["dropdown-menu--is-open"]}`
+  //             : `${styles["dropdown-menu--is-close"]}`
+  //         } `}
+  //       >
+  //         <li className={`${styles["nav__item--mobile"]}`}>
+  //           <Link href="#">
+  //             <div onClick={showMenu}>
+  //               <FontAwesomeIcon icon={faHome} />
+  //               <a> Home</a>
+  //             </div>
+  //           </Link>
+  //         </li>
+  //         <li className={`${styles["nav__item--mobile"]}`}>
+  //           <Link href="#servizi">
+  //             <div onClick={showMenu}>
+  //               <FontAwesomeIcon icon={faBriefcase} />
+  //               <a> Servizi</a>
+  //             </div>
+  //           </Link>
+  //         </li>
+  //         <li className={`${styles["nav__item--mobile"]}`}>
+  //           <Link href="#">
+  //             <div>
+  //               <FontAwesomeIcon icon={faUserFriends} />
+  //               <a> Chi siamo</a>
+  //             </div>
+  //           </Link>
+  //         </li>
+  //         <li className={`${styles["nav__item--mobile"]}`}>
+  //           <Link href="#">
+  //             <div>
+  //               <FontAwesomeIcon icon={faEnvelopeOpenText} />
+  //               <a> Contatti</a>
+  //             </div>
+  //           </Link>
+  //         </li>
+  //       </div>
+  //     </>
+  //   );
+  // };
 
-return (
-  <header className={`${styles['header']}`}>
-    <Image className={`${styles['header__logo']}`} src='/images/alarm.png' width={50} height={50} />
-    <nav className={`${styles['header__navigation']}`}>
-      <ul className={`${styles['header__navigation-list']}`}>
-        <li className={`${styles['header__navigation-item']}`}>servizi</li>
-        <li className={`${styles['header__navigation-item']}`}>chi siamo</li>
-        <li className={`${styles['header__navigation-item']}`}>gallery</li>
-        <li className={`${styles['header__navigation-item']}`}>contatti</li>
-      </ul>
-    </nav>
-    <Button
-      settings={
-        {
-          text: 'chiamaci',
-          type: 'button',
-          customClass: `${styles['header__button']}`,
-        }
-      }
-    />
-  </header>
-);
-}
+  return (
+    <header className={`${styles["header"]}`}>
+      <Image
+        className={`${styles["header__logo"]}`}
+        src="/images/alarm.png"
+        width={50}
+        height={50}
+      />
+      <nav className={`${styles["header__navigation"]}`}>
+        <ul className={`${styles["header__navigation-list"]}`}>
+          <li className={`${styles["header__navigation-item"]}`}>servizi</li>
+          <li className={`${styles["header__navigation-item"]}`}>chi siamo</li>
+          <li className={`${styles["header__navigation-item"]}`}>gallery</li>
+          <li className={`${styles["header__navigation-item"]}`}>contatti</li>
+        </ul>
+      </nav>
+      <Button
+        settings={{
+          text: "399 1112233",
+          type: "button",
+          customClass: `${styles["header__button"]}`,
+          customClassText: `${styles["header__button-text"]}`
+        }}
+      />
+    </header>
+  );
+};
 
 export default Header;
