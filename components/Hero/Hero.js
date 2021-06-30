23f544b0699deb8aaec260831f2e8ce575a2579c
Andrea Bruno <andreabrunoxy@gmail.com>
2021-06-30 08:06:49 +0200

    Add Hero section

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -2,30 +2,29 @@ import styles from "./Hero.module.css";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // Import the FontAwesomeIcon component
-import { faArrowDown, faPhone } from "@fortawesome/free-solid-svg-icons"; // import the icons you need
-import { Button } from "../Button/Button";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons"; // import the icons you need
 import { Fade, Flip } from "react-reveal";
 
 const Hero = () => {
   return (
     <div className={`${styles["hero"]}`}>
-      <Fade left delay={800}>
+      <Fade left>
         <div className={`${styles["hero__heading"]}`}>
           <h1 className={`${styles["hero__title"]}`}>CE.MA Impianti</h1>
           <h2 className={`${styles["hero__subtitle"]}`}>
             Impianti elettrici civili e industriali
           </h2>
-          <Fade up delay={1600}>
+          <Fade up delay={800}>
             <button className={`${styles["hero__button"]}`} type="button">
               <a href="#servizi">
-                <FontAwesomeIcon icon={faArrowDown} /> Scopri di più
+                <FontAwesomeIcon icon={faArrowDown} size="lg" /> Scopri di più
               </a>
             </button>
           </Fade>
         </div>
       </Fade>
       <Fade>
-        <Flip top>
+        <Flip top delay={1000}>
           <div className={`${styles["hero__image-container"]}`}>
             <Image
               className={`${styles["hero__image"]}`}
