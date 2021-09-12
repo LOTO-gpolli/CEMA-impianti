8e26db6e53d2465e396f3b8c54e3d951f3630664
gpolli.dev <gpolli.dev@gmail.com>
2021-09-12 17:45:52 +0200

    feat: add lint and format during pre-commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <>
-      <div>Header</div>
-      <div>Hero</div>
-      <div>Services</div>
+            <div>Header</div>
+            <div>Hero</div>
+              <div>Services</div>
       <div>About us</div>
       <div>Gallery</div>
       <div>Form</div>
