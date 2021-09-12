28d5be57031905895cc28394bcc319a0ffe6fe15
gpolli.dev <gpolli.dev@gmail.com>
2021-09-12 11:01:30 +0200

    style: cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,14 @@
-import Head from "next/head";
-/* Components */
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
-import Hero from "../components/Hero/Hero";
-import CardsList from "../components/CardsList/CardsList";
-import About from "../components/About/About";
-import Form from "../components/Form/Form";
-import Gallery from "../components/Gallery/Gallery";
-import Copyright from "../components/Copyright/Copyright";
-
 export default function Home() {
   return (
     <>
-      <Header />
-      <Hero />
-      <CardsList />
-      <About />
-      <Gallery />
-      <Form />
-      <Footer />
-      <Copyright />
+      <div>Header</div>
+      <div>Hero</div>
+      <div>Services</div>
+      <div>About us</div>
+      <div>Gallery</div>
+      <div>Form</div>
+      <div>Footer</div>
+      <div>Copyright</div>
     </>
   );
 }
