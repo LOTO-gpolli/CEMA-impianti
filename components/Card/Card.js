2e4ce29e4a69bafd7b9096a011f5d0a260ee6bc6
Andrea Bruno <andreabrunoxy@gmail.com>
2021-09-15 11:06:15 +0200

    Add minor changes to Cards Section

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,15 +1,15 @@
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import styles from "../Card/Card.module.css";
-import Fade from "react-reveal";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import styles from '../Card/Card.module.css';
+import Fade from 'react-reveal';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import the FontAwesomeIcon component
 
 const Card = ({
   settings = {
     icon: {},
     // alt: "",
-    title: "",
-    text: ""
+    title: '',
+    text: ''
   }
 }) => {
   const { title, text, icon } = settings;
@@ -17,34 +17,34 @@ const Card = ({
   const [underline, setUnderline] = useState(false);
 
   const changeUL = () => {
-    if (window.scrollY > 600) {
+    if (window.scrollY > 500) {
       setUnderline(true);
     } else {
       setUnderline(false);
     }
   };
   useEffect(() => {
-    window.addEventListener("scroll", changeUL);
+    window.addEventListener('scroll', changeUL);
   });
 
   return (
     <>
       <Fade bottom>
-        <div className={`${styles["card__container"]}`}>
-          <div className={styles["card__icon-container"]}>
-            <FontAwesomeIcon icon={icon} size={"2x"} className={styles["card__icon"]} />
+        <div className={`${styles['card__container']}`}>
+          <div className={styles['card__icon-container']}>
+            <FontAwesomeIcon icon={icon} size={'2x'} className={styles['card__icon']} />
           </div>
           <Fade cascade>
-            <div className={styles["card__content"]}>
+            <div className={styles['card__content']}>
               <h3
                 className={
-                  underline ? styles["card__title-scroll"] : styles["card__title"]
+                  underline ? styles['card__title-scroll'] : styles['card__title']
                 }
               >
                 {title}
               </h3>
 
-              <p className={styles["card__subtitle"]}>{text}</p>
+              <p className={styles['card__subtitle']}>{text}</p>
             </div>
           </Fade>
         </div>
