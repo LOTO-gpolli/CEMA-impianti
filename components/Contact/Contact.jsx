bd878fcab6db0be717addd0b169d40cb7f9793f9
gpolli.dev <gpolli.dev@gmail.com>
2021-10-17 17:23:50 +0200

    feat: add SectionHeading

---
+++ components/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import SectionHeading from '../SectionHeading/SectionHeading';
+import ContactForm from '../ContactForm/ContactForm';
+
+const Contact = () => {
+  return (
+    <>
+      <SectionHeading title="Contatti" />
+      <ContactForm />
+    </>
+  );
+}
+
+export default Contact
---
+++ components/SectionHeading/SectionHeading.jsx
@@ -0,0 +1,12 @@
+import styles from './SectionHeading.module.css'
+
+const SectionHeading = ({ title, subtitle = "" }) => {
+  return (
+    <div className={`${styles['section-heading']}`}>
+      <h3 className={`${styles['section-heading__title']}`}>{title}</h3>
+      {subtitle && <h4 className={`${styles['section-heading__subtitle']}`}>{subtitle}</h4>}
+    </div>
+  );
+}
+
+export default SectionHeading
