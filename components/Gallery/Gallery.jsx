3dadfcb14c64199fb418f7e7b338bda0ed31ef11
gpolli.dev <gpolli.dev@gmail.com>
2022-01-30 15:36:11 +0100

    feat: create Timeline

---
+++ components/Gallery/Gallery.jsx
@@ -0,0 +1,13 @@
+import SectionHeading from '../SectionHeading/SectionHeading';
+import Timeline from '../Timeline/Timeline';
+
+const Gallery = () => {
+  return (
+    <section>
+      <SectionHeading title="I nostri progetti" />
+      <Timeline />
+    </section>
+  );
+}
+
+export default Gallery
--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,13 @@ import { useEffect, useState } from 'react';
 import Header from '../components/Header/Header';
 import Hero from '../components/Hero/Hero';
 import About from '../components/About/About';
-import Timeline from '../components/Timeline/Timeline';
 import Contact from '../components/Contact/Contact';
+import Gallery from '../components/Gallery/Gallery';
 import Footer from '../components/Footer/Footer';
 import Services from '../components/Services/Services';
 import MobilePhoneButton from '../components/MobilePhoneButton/MobilePhoneButton';
 
 export default function Home() {
-  const [showGallery, setShowGallery] = useState(false);
-
-  useEffect(() => {
-    if (document?.location?.search?.includes('showGallery=true')) {
-      setShowGallery(true);
-    }
-  });
-
   return (
     <>
       <Header />
@@ -25,7 +17,7 @@ export default function Home() {
       <Hero />
       <Services />
       <About />
-      {/* {showGallery && <Timeline />} */}
+      <Gallery />
       <Contact />
       <Footer />
     </>
