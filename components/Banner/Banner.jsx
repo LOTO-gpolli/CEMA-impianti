9f36e5e1f80f11bc2c61f39f9746db3bdbba7cfd
Andrea <andreabrunoxy@gmail.com>
2022-02-15 17:17:43 +0100

    feat: use Image in Banner

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -1,9 +1,12 @@
 import styles from './Banner.module.css'
+import Image from "next/image"
 
 const Banner = () => {
   return (
     <div className={`${styles['banner']}`}>
-      <img className={`${styles['banner__icon']}`} src='/images/IMQ_certification.png' />
+      <div>
+      <Image className={`${styles['banner__icon']}`} src='/images/IMQ_certification.png' alt='IMQ certification' width={75} height={75} />
+      </div>
       <div className={`${styles['banner__text-wrapper']}`}>
         <p className={`${styles['banner__title']}`}>Installatori certificati IMQ-AIR</p>
         <cite className={`${styles['banner__citation']}`}>Certificazione esperto impianti di allarme intrusione e rapina</cite>
