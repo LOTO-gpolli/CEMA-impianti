93329468f0449fcf13ceeff2b7a7637dfee78800
gpolli.dev <gpolli.dev@gmail.com>
2022-02-27 12:43:20 +0100

    feat: redesign IMQ banner

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -1,9 +1,20 @@
-import styles from './Banner.module.css'
+import Image from 'next/image';
+
+import styles from './Banner.module.css';
 
 const Banner = () => {
   return (
     <div className={`${styles['banner']}`}>
-      <img className={`${styles['banner__icon']}`} src='/images/IMQ_certification.png' />
+      <div className={`${styles['banner__icon-wrapper']}`}>
+        <Image
+          className={`${styles['banner__icon']}`}
+          src='/images/IMQ_certification.png'
+          alt='IMQ certification'
+          width={60}
+          height={60}
+          layout='fixed'
+        />
+      </div>
       <div className={`${styles['banner__text-wrapper']}`}>
         <p className={`${styles['banner__title']}`}>Installatori certificati IMQ-AIR</p>
         <cite className={`${styles['banner__citation']}`}>Certificazione esperto impianti di allarme intrusione e rapina</cite>
