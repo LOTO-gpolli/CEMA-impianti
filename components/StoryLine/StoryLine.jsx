c7dbd9fdb390ee455c983b2352c3b45589e30491
Andrea <andreabrunoxy@gmail.com>
2022-02-28 11:57:24 +0100

    feat: use Next Image instead of img

--- components/StoryLine/StoryLine.jsx
+++ components/StoryLine/StoryLine.jsx
@@ -1,16 +1,27 @@
 // Styles
 import styles from './StoryLine.module.css';
+import Image from 'next/image';
 
-const StoryLine = ({ description, imageSource, title = '', reverse = false  }) => {
+const StoryLine = ({ description, imageSource, title = '', reverse = false }) => {
   return (
-    <div className={`${styles['storyline']} ${ reverse ? `${styles['storyline--reverse']}` : '' }`}>
+    <div className={`${styles['storyline']} ${reverse ? `${styles['storyline--reverse']}` : ''}`}>
       <div className={styles['storyline__text-wrapper']}>
         {title && <h4 className={styles['storyline__title']}>{title}</h4>}
         <p className={styles['storyline__description']}>{description}</p>
       </div>
-      <img className={styles['storyline__image']} src={imageSource} />
+      <div className={styles['storyline__image-wrapper']}>
+        <Image
+          className={styles['storyline__image']}
+          src={imageSource}
+          width={6}
+          height={4}
+          layout="responsive"
+        />
+      </div>
+
+      {/* <img className={styles['storyline__image']} src={imageSource} /> */}
     </div>
   );
-}
+};
 
 export default StoryLine;
