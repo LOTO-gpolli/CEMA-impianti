cbebf8450d9b55a43a3fd9af2c799b3811f74c89
Gabriele Polli <gpolli.dev@gmail.com>
2024-03-04 16:46:52 +0100

    feat: 🎸 add feedback after form submit

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -1,26 +1,30 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import SectionHeading from '../SectionHeading/SectionHeading';
 import ContactForm from '../ContactForm/ContactForm';
+import Modal from '../Modal/Modal';
 
 import styles from './Contact.module.css';
 
 const Contact = ({ title, subtitle, subjectOptions, privacy }) => {
+  const [modalText, setModalText] = useState('');
+
+  const closeModal = () => setModalText('');
+
   return (
     <section id="contacts" className={`${styles['contact']}`}>
-      <SectionHeading
-        title={title}
-        subtitle={subtitle}
-      />
-      <ContactForm subjectOptions={subjectOptions} privacy={privacy} />
+      <Modal open={modalText !== ''} onClose={closeModal} text={modalText} />
+      <SectionHeading title={title} subtitle={subtitle} />
+      <ContactForm setModalText={setModalText} subjectOptions={subjectOptions} privacy={privacy} />
     </section>
   );
-}
+};
 
 Contact.propTypes = {
   title: PropTypes.string.isRequired,
   subtitle: PropTypes.string,
   subjectOptions: PropTypes.arrayOf(PropTypes.string).isRequired,
   privacy: PropTypes.string.isRequired,
-}
+};
 
-export default Contact
+export default Contact;
